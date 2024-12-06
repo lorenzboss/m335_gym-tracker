4173448a796efc74a62fdf8f9929760a77a7cffd
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-12-06 15:30:16 +0100

    Add second tap where you see the current battery level.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Device } from '@capacitor/device'; // Importiere Device Plugin
+import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,10 +8,32 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab2Page {
+  batteryLevel: number = 0;
+  isCharging: boolean = false;
 
-  constructor() {}
+  constructor() {
+    // Hole den Akku-Status bei der Initialisierung
+    this.getBatteryStatus();
 
+    // Setze ein Intervall, um den Status alle 5 Sekunden zu aktualisieren
+    setInterval(() => {
+      this.getBatteryStatus();
+    }, 5000); // alle 5000ms (5 Sekunden)
+  }
+
+  async getBatteryStatus() {
+    try {
+      const batteryInfo = await Device.getBatteryInfo();
+      this.batteryLevel =
+        batteryInfo.batteryLevel !== undefined
+          ? batteryInfo.batteryLevel * 100
+          : 0;
+      this.isCharging = batteryInfo.isCharging ?? false;
+    } catch (error) {
+      console.error('Fehler beim Abrufen des Akku-Status', error);
+    }
+  }
 }
