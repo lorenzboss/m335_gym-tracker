dc627faeefaa92439ed67c848eb854f05c8e0b6e
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-12-09 13:46:48 +0100

    Create new statistics page with static temporary data.

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule, RefresherCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.page.html',
   styleUrls: ['./statistics.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class StatisticsPage implements OnInit {
+export class StatisticsPage {
+  stats = {
+    visitsThisWeek: 5,
+    lastGymLocation: 'Fitness Studio Basel',
+    streak: 3,
+    progress: 0.7,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  refreshStats(event: RefresherCustomEvent) {
+    console.log('Refreshing statistics...');
+
+    // Simuliere das Laden von Daten
+    setTimeout(() => {
+      console.log('Statistics refreshed');
+      event.target.complete(); // Schließe den Refresher
+    }, 2000);
   }
-
 }
