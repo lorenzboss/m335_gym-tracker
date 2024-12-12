8cc01e98ebaa16ca7e4867d4b87d205d780cbb52
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-12-12 14:09:21 +0100

    Add gym management features to settings page with CRUD operations

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import { globeOutline, mailOutline } from 'ionicons/icons';
+import {
+  globeOutline,
+  mailOutline,
+  pencilOutline,
+  trashOutline,
+} from 'ionicons/icons';
+import { GymService } from '../gym.service';
+import { Gym } from '../models/gym.model';
 import { NotificationsService } from '../notifications.service';
 import { ThemeService } from '../theme.service';
 
@@ -17,19 +24,146 @@ type Frequency = 'daily' | 'weekly' | 'monthly';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class SettingsPage implements OnInit {
+  gyms: Gym[] = [];
   darkMode = false;
   notificationsEnabled = false;
   notificationFrequency: Frequency = 'daily';
 
   constructor(
+    private gymService: GymService,
     private themeService: ThemeService,
-    private notificationsService: NotificationsService
+    private notificationsService: NotificationsService,
+    private alertController: AlertController
   ) {
-    addIcons({ mailOutline, globeOutline });
+    addIcons({ mailOutline, globeOutline, pencilOutline, trashOutline });
   }
 
-  ngOnInit() {
-    this.initializeSettings();
+  async ngOnInit() {
+    await this.initializeSettings();
+    await this.loadGyms();
+  }
+
+  async loadGyms() {
+    try {
+      this.gyms = await this.gymService.getGyms();
+    } catch (error) {
+      console.error('Error loading gyms:', error);
+    }
+  }
+
+  async refreshLogs(event?: any) {
+    try {
+      await this.loadGyms();
+
+      if (event) {
+        event.target.complete();
+      }
+    } catch (error) {
+      console.error('Fehler beim Aktualisieren der Gyms:', error);
+      if (event) {
+        event.target.complete();
+      }
+    }
+  }
+
+  async openAddGymModal() {
+    const alert = await this.alertController.create({
+      header: 'Add New Gym',
+      inputs: [
+        {
+          name: 'gymName',
+          type: 'text',
+          placeholder: 'Enter gym name',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+        {
+          text: 'Add',
+          handler: (data) => {
+            const gymName = data.gymName;
+            if (gymName) {
+              this.addGym(gymName);
+            }
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async openEditGymModal(gym: Gym) {
+    const alert = await this.alertController.create({
+      header: 'Edit Gym',
+      inputs: [
+        {
+          name: 'gymName',
+          type: 'text',
+          value: gym.name, // Pre-fill with the current name of the gym
+          placeholder: 'Enter new gym name',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+        {
+          text: 'Save',
+          handler: (data: { gymName: any }) => {
+            const newName = data.gymName;
+            if (newName) {
+              this.updateGym(gym, newName);
+            }
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async addGym(gymName: string) {
+    try {
+      await this.gymService.addGym({
+        name: gymName,
+      });
+      await this.loadGyms();
+    } catch (error) {
+      console.error('Error adding gym:', error);
+    }
+  }
+
+  async updateGym(gym: Gym, newName: string) {
+    try {
+      await this.gymService.updateGym(gym.id!, { name: newName });
+      await this.loadGyms();
+    } catch (error) {
+      console.error('Error updating gym:', error);
+    }
+  }
+
+  async deleteGym(id: string) {
+    if (confirm('Are you sure you want to delete this gym?')) {
+      try {
+        await this.gymService.deleteGym(id);
+        await this.loadGyms();
+      } catch (error) {
+        console.error('Error deleting gym:', error);
+      }
+    }
   }
 
   private async initializeSettings() {
